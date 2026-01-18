b2fbd363af0c9dee6bbb7c0b7ebdc89962695b50
Hardik Teotia <hardik10teotia@gmail.com>
2026-01-18 09:41:17 +0530

    project setup

---
+++ FitPro/src/main/java/com/fitpro/backend/FitproApplication.java
@@ -0,0 +1,13 @@
+package com.fitpro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitproApplication.class, args);
+	}
+
+}
